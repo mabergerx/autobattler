3fb83cc3f6a8de81bcf02dc9875df969fd60b726
mark <maberger0811@gmail.com>
2023-04-28 09:35:28 +0200

    Update card_interactions.py

--- card_interactions.py
+++ card_interactions.py
@@ -8,15 +8,29 @@ def attack(attacker, defender):
     attacker_ds = attacker.divine_shield
     defender_ds = defender.divine_shield
 
-    def determine_single_attack_dmg():
-        damage_to_defender = 
+    def determine_health_delta_after_attack(attacker_divine_shield, defender_divine_shield):
+
+        # TODO: Remember to update divine shield status after this resolves!
+
+        if defender_divine_shield:
+            defender_health_delta = 0
+        else:
+            defender_health_delta = attacker_attack_stat
+
+        if attacker_divine_shield:
+            attacker_health_delta = 0
+        else:
+            attacker_health_delta = defender_attack_stat
+
+        return defender_health_delta, attacker_health_delta
 
     # For now we disregard windfury as it doesn't appear in our card set
     if defender_ds:
         if attacker_ds:
             # Nothing happens, as divine shields cancel each other out
-            attacker.adjust_health(0)
-            defender.adjust_health(0)
+            defender_delta, attacker_delta = determine_health_delta_after_attack(attacker_ds, defender_ds)
+            attacker.adjust_health(attacker_delta)
+            defender.adjust_health(defender_delta)
         else:
             # Attacker takes damage, while the defender does not
 
