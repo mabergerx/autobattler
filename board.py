ab01d03730f1bbb55fa6efc67769b993f99e604c
mark <maberger0811@gmail.com>
2023-04-28 10:39:34 +0200

    Update board.py

--- board.py
+++ board.py
@@ -11,22 +11,14 @@ class Board:
         # TODO: how? do we wanna keep internal card ids per board?
         pass
 
-    # def __str__(self):
-    #     cards = "\n       ".join(
-    #         [
-    #             f"{card.name} ({card.attack}/{card.health}) [DS:{card.divine_shield} / Taunt:{card.taunt}]"
-    #             for card in self.cards
-    #         ]
-    #     )
-    #     return f"Player: {self.player}\nCards: {cards}"
-
     def __str__(self):
         board_str = f"Player: {self.player}\n"
 
         for card in self.cards:
             card_repr = f"{card.name} ({card.attack}/{card.health}) [DS:{card.divine_shield} / Taunt:{card.taunt}]"
-            board_str += "|{0:-^48}|\n".format("")
-            board_str += "|{0:^48}|\n".format(card_repr)
-            board_str += "|{0:-^48}|\n".format("")
+            border_len = len(card_repr) + 4  # Adding 2 extra dashes on both sides
+            board_str += "|{0:-^{border_len}}|\n".format("", border_len=border_len)
+            board_str += "|  {0:{content_len}}|\n".format(card_repr, content_len=border_len - 2)
+            board_str += "|{0:-^{border_len}}|\n".format("", border_len=border_len)
 
         return board_str
